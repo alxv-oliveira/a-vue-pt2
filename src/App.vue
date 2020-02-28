a6294aadecede1b977d7faa5e4f9feee5defef41
Alexander Victor <alxv.oliveira@gmail.com>
2020-02-28 08:01:49 -0300

    06.Single Page Application e a criação de rotas

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,25 @@
 <template>
     <div class="corpo">
-        <h1 class="centralizado">{{ titulo }}</h1>
-
-        <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtre por parte do titulo">
-        <ul class="lista-fotos">
-            <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
-                <meu-painel :titulo="foto.titulo">
-                    <imagem-responsiva :url="foto.url" :titulo="foto-titulo" />
-                </meu-painel>
-            </li>
-        </ul>
+        <meu-menu :rotas="routes" />
+        <transition name="pagina">
+            <router-view></router-view>
+        </transition>
     </div>
 </template>
 
 <script>
-import Painel from './components/shared/painel/Painel';
-import ImagemResponsiva from './components/shared/imagem-responsiva/imagemResponsiva';
+import { routes } from './routes';
+import Menu from './components/shared/menu/Menu';
 
 export default {
     components: {
-        'meu-painel': Painel,
-        'imagem-responsiva': ImagemResponsiva
+        'meu-menu': Menu,
     },
 
     data() {
         return {
-            titulo: "Alurapic",
-            fotos: [],
-            filtro: '',
-        };
-    },
-
-    computed: {
-        fotosComFiltro() {
-            if(this.filtro) {
-                let exp = new RegExp(this.filtro.trim(), 'i');
-
-                return this.fotos.filter(foto => exp.test(foto.titulo));
-            } else {    
-                return this.fotos;
-            }
+            routes
         }
-    },
-
-    created() {
-        this.$http
-            .get("http://localhost:3000/v1/fotos")
-            .then(res => res.json())
-            .then(
-                fotos => (this.fotos = fotos),
-                err => console.log(err)
-            );
     }
 };
 </script>
@@ -62,22 +31,11 @@ export default {
         margin: 0 auto;
     }
 
-    .centralizado {
-        text-align: center;
+    .pagina-enter, .pagina-leave-active {
+        opacity: 0;
     }
 
-    .lista-fotos {
-        list-style: none;
-    }
-
-    .lista-fotos .lista-fotos-item {
-        display: inline-block;
-    }
-
-
-
-    .filtro {
-        display: block;
-        width: 100%;
+    .pagina-enter-active, .pagina-leave-active {
+        transition: opacity .4s;
     }
 </style>
