f51b04751a5d9724de70a19ad07f7e4a45374376
Alexander Victor <alxv.oliveira@gmail.com>
2020-02-28 07:09:06 -0300

    05.Melhorando a UX com animações

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
     <div class="corpo">
         <h1 class="centralizado">{{ titulo }}</h1>
 
+        <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtre por parte do titulo">
         <ul class="lista-fotos">
-            <li class="lista-fotos-item" v-for="foto of fotos">
+            <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
                 <meu-painel :titulo="foto.titulo">
-                    <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+                    <imagem-responsiva :url="foto.url" :titulo="foto-titulo" />
                 </meu-painel>
             </li>
         </ul>
@@ -14,17 +15,34 @@
 
 <script>
 import Painel from './components/shared/painel/Painel';
+import ImagemResponsiva from './components/shared/imagem-responsiva/imagemResponsiva';
+
 export default {
     components: {
         'meu-painel': Painel,
+        'imagem-responsiva': ImagemResponsiva
     },
 
     data() {
         return {
             titulo: "Alurapic",
-            fotos: []
+            fotos: [],
+            filtro: '',
         };
-  },
+    },
+
+    computed: {
+        fotosComFiltro() {
+            if(this.filtro) {
+                let exp = new RegExp(this.filtro.trim(), 'i');
+
+                return this.fotos.filter(foto => exp.test(foto.titulo));
+            } else {    
+                return this.fotos;
+            }
+        }
+    },
+
     created() {
         this.$http
             .get("http://localhost:3000/v1/fotos")
@@ -56,7 +74,10 @@ export default {
         display: inline-block;
     }
 
-    .imagem-responsiva {
+
+
+    .filtro {
+        display: block;
         width: 100%;
     }
 </style>
