de6e4d8567eeef3045325b23ce69a0160fb66f71
Alexander Victor <alxv.oliveira@gmail.com>
2020-03-14 20:26:31 -0300

    Documentação adicionada

--- src/routes.js
+++
@@ -1,7 +0,0 @@
-import Cadastro from './components/cadastro/Cadastro';
-import Home from './components/home/Home';
-
-export const routes = [
-    { path: '', component: Home , titulo: 'Home'},
-    { path: '/cadastro', component: Cadastro, titulo: 'Cadastro' },
-];
